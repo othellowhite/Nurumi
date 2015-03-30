071859ba69ef39b10f5fe39704ceadada0512a20
Park, Hyung Soon <starvesperia@naver.com>
2015-03-30 18:55:17 +0900

    custom keyboard testing
    Custom input method test

---
+++ Nurumikeyboard/src/com/fouram/nurumikeyboard/NurumiIME/NurumiIME.java
@@ -0,0 +1,94 @@
+package com.fouram.nurumikeyboard.NurumiIME;
+
+
+import android.inputmethodservice.InputMethodService;
+import android.inputmethodservice.Keyboard;
+import android.inputmethodservice.KeyboardView;
+import android.inputmethodservice.KeyboardView.OnKeyboardActionListener;
+import android.view.KeyEvent;
+import android.view.View;
+import android.view.inputmethod.InputConnection;
+
+public class NurumiIME extends InputMethodService implements
+		OnKeyboardActionListener {
+
+	private KeyboardView kv;
+	private Keyboard keyboard;
+	private boolean caps = false;
+	
+	@Override
+	public View onCreateInputView() {
+	    kv = (KeyboardView)getLayoutInflater().inflate(R.layout.keyboard, null);
+	    keyboard = new Keyboard(this, R.xml.qwerty);
+	    kv.setKeyboard(keyboard);
+	    kv.setOnKeyboardActionListener(this);
+	    return kv;
+	}
+	
+	@Override
+	public void onPress(int primaryCode) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void onRelease(int primaryCode) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void onKey(int primaryCode, int[] keyCodes) {       
+	    InputConnection ic = getCurrentInputConnection();
+	    switch(primaryCode){
+	    case Keyboard.KEYCODE_DELETE :
+	        ic.deleteSurroundingText(1, 0);
+	        break;
+	    case Keyboard.KEYCODE_SHIFT:
+	        caps = !caps;
+	        keyboard.setShifted(caps);
+	        kv.invalidateAllKeys();
+	        break;
+	    case Keyboard.KEYCODE_DONE:
+	        ic.sendKeyEvent(new KeyEvent(KeyEvent.ACTION_DOWN, KeyEvent.KEYCODE_ENTER));
+	        break;
+	    default:
+	        char code = (char)primaryCode;
+	        if(Character.isLetter(code) && caps){
+	            code = Character.toUpperCase(code);
+	        }
+	        ic.commitText(String.valueOf(code),1);                 
+	    }
+	}
+
+	@Override
+	public void onText(CharSequence text) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void swipeLeft() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void swipeRight() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void swipeDown() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void swipeUp() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
